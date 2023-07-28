64483a540f1ae29140a9b330d1745408b72ef719
Renan Jochem <renanjochem98@outlook.com>
2023-07-27 21:48:31 -0300

    Altera footer para ficar preso ao final da paginal e criar componente para header.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,23 @@
 // import { useState } from 'react'
 import './App.css'
+import Header  from './Components/Header'
 import BtnHeader  from './Components/BtnHeader'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-gradient-to-r from-primaria  via-secundaria to-primaria'>
-      <header>
-        <div className='text-center font-bold text-primaria w-full p-4 text-xl shadow-md shadow-black/30'>
-           Administrador Financeiro Pessoal
-        </div>
-      </header>
-      <main>
+    <div>
+      <Header />
+      <main className="">
         <nav className='mx-auto flex items-center justify-between p-6'>
           <BtnHeader texto="Adicionar entrada" />
           <BtnHeader texto="Adicionar Saída"/>
         </nav>
         Hello World, de novo
       </main>
-      <footer>
+      <footer className='fixed bottom-0 text-center font-bold bg-gradient-to-r from-primaria  via-secundaria to-primaria
+         text-primaria w-full p-4 text-xl shadow-md shadow-black/30'>
         Footer
       </footer>
     </div>
---
+++ frontend/src/Components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <div className='text-center font-bold bg-gradient-to-r from-primaria  via-secundaria to-primaria
+            text-primaria w-full p-4 text-xl shadow-md shadow-black/30'>
+            Administrador Financeiro Pessoal
+            </div>
+      </header>
+    )
+}
