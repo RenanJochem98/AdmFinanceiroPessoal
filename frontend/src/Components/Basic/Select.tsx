d0e2694e1264e776e23dc02116d45d6d45172433
Renan Jochem <renanjochem98@outlook.com>
2023-09-08 13:42:34 -0300

    Adicion 'key' às opções do select.

--- frontend/src/Components/Basic/Select.tsx
+++ frontend/src/Components/Basic/Select.tsx
@@ -11,9 +11,9 @@ export default function Selec({ placeholder, options}: ISelectProps){
     return (
         <>
             <select className="w-full border-b-2 my-2">
-                <option value="" >{placeholder}</option>
+                <option value="" key={0} >{placeholder}</option>
                 {options.map(opt =>{
-                    return <option value={opt.value}>{opt.text}</option>
+                    return <option key={opt.value} value={opt.value}>{opt.text}</option>
                 })}
             </select>
         </>
