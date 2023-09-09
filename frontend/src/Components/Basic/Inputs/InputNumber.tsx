365be2bcdba6d68fa3fa39ce2f3aa51b6d6bfbe6
Renan Jochem <renanjochem98@outlook.com>
2023-09-09 11:01:03 -0300

    Tentativa de alteração de componente de input para aceitar react form hook. Não funcionou.

--- frontend/src/Components/Basic/Inputs/InputNumber.tsx
+++ frontend/src/Components/Basic/Inputs/InputNumber.tsx
@@ -1,13 +1,21 @@
-interface IInputNumberProps {
-    placeholder?: string
+// import * as React from "react";
+import { FieldValues, UseFormRegister } from "react-hook-form";
+import { IDefaultInput } from "../../../types/IDefaultInput"
+
+interface IInputNumberProps extends IDefaultInput {
+    register?: UseFormRegister<FieldValues>
+    // registerName?: string
 }
-export default function InputNumber({ placeholder}: IInputNumberProps){
+// Estudar sobre React.forwardRef 
+export default function InputNumber({register, ...rest}: IInputNumberProps){
     return (
         <>
             <input
+                // {...rest}
+                {...(register )}
                 type="number" 
-                placeholder={placeholder} 
-                className="border-b-2 w-full my-2"
+                placeholder={rest.placeholder} 
+                className={"border-b-2 w-full my-2 " + (rest.className == null ? "" : rest.className)}
             />
         </>
     )
