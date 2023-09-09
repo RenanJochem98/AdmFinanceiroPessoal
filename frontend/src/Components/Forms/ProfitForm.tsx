90e329990c6aa87f756e25e4b16af4a0ee9d5651
Renan Jochem <renanjochem98@outlook.com>
2023-09-09 11:59:10 -0300

    Adiciona validações ao formulário.

--- frontend/src/Components/Forms/ProfitForm.tsx
+++ frontend/src/Components/Forms/ProfitForm.tsx
@@ -20,25 +20,35 @@ export default function ProfitForm(){
     const {
         register,
         handleSubmit,
-        // formState: { errors },
+        formState: { errors },
     } = useForm<IProfitFormSend>()
 
-    const onSubmit: SubmitHandler<IProfitFormSend> = (data) => console.log(data)
+    const onSubmit: SubmitHandler<IProfitFormSend> = (data) => {
+        console.log(data)
+        console.log(errors)
+    }
 
     return (
-        <div className="flex flex-col p-5 items-center text-center">
-                    {/* <div className="text-center">Terste</div> */}
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <input type="number" placeholder="Valor" className="border-b-2 w-full my-2 " {...register("value")}/>
-                        <input type="date" placeholder="Data" className="border-b-2 w-full my-2 " {...register("date")}/>
-                        <select className="w-full border-b-2 my-2" {...register("account")}>
-                            <option value="" key={0} >Conta</option>
-                            {userAccounts.map(opt =>{
-                                return <option key={opt.value} value={opt.value}>{opt.text}</option>
-                            })}
-                        </select>
-                        <ConfirmButton type="submit" className="mt-2 justify-center" />
-                    </form>
-                </div>
+        <div className="flex flex-col p-5 items-center">
+            {/* <div className="text-center">Terste</div> */}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="number" placeholder="Valor" className="border-b-2 w-full mt-2 text-center" {...register("value", {required: true,})}/>
+                {errors.value &&<span className="text-xs text-red-700 font-semibold">Este campo é obriatório.</span>}
+                {/* <p >Erro</p> */}
+               
+                <input type="date" placeholder="Data" className="border-b-2 w-full mt-2 " {...register("date", {required: true,})}/>
+                {errors.date &&<span className="text-xs text-red-700 font-semibold">Este campo é obriatório.</span>}
+                
+                <select className="w-full border-b-2 mt-2" {...register("account", {required: true,})}>
+                    <option value="" key={0} >Conta</option>
+                    {userAccounts.map(opt =>{
+                        return <option key={opt.value} value={opt.value}>{opt.text}</option>
+                    })}
+                </select>
+                {errors.account &&<span className="text-xs text-red-700 font-semibold">Este campo é obriatório.</span>}
+                
+                <ConfirmButton type="submit" className="w-full mt-10 justify-center" />
+            </form>
+        </div>
     )
 }
